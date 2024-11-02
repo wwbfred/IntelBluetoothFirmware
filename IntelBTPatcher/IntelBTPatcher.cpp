4726d83239b10c9be268b38dd2ff672a190f18ec
Fred Wu <wwbfred@gmail.com>
2024-11-02 08:56:29 +0800

    Update IntelBTPatcher.cpp

--- IntelBTPatcher/IntelBTPatcher.cpp
+++ IntelBTPatcher/IntelBTPatcher.cpp
@@ -167,8 +167,8 @@ IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, 
     if (hdr) {
         if (hdr->opcode != HCI_OP_LE_SET_SCAN_PARAM && hdr->opcode != HCI_OP_LE_SET_SCAN_ENABLE)
             ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, data, descriptor, length, completion, timeout);
-        else
         #if DEBUG
+        else
             DBGLOG(DRV_NAME, "Disable Scan!");
         #endif
             
