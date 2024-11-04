345248be09a7ea4aedced1f335b54478348b22fd
Fred Wu <wwbfred@gmail.com>
2024-11-04 09:43:46 +0800

    Update IntelBTPatcher.cpp

--- IntelBTPatcher/IntelBTPatcher.cpp
+++ IntelBTPatcher/IntelBTPatcher.cpp
@@ -43,17 +43,6 @@ PluginConfiguration ADDPR(config) {
     }
 };
 
-static const char *IntelBTPatcher_IOBluetoothFamily[] { "/System/Library/Extensions/IOBluetoothFamily.kext/Contents/MacOS/IOBluetoothFamily" };
-
-static KernelPatcher::KextInfo IntelBTPatcher_IOBluetoothInfo {
-    "com.apple.iokit.IOBluetoothFamily",
-    IntelBTPatcher_IOBluetoothFamily,
-    1,
-    {true, true},
-    {},
-    KernelPatcher::KextInfo::Unloaded
-};
-
 static const char *IntelBTPatcher_IOUSBHostFamily[] {
     "/System/Library/Extensions/IOUSBHostFamily.kext/Contents/MacOS/IOUSBHostFamily" };
 
@@ -66,23 +55,14 @@ static KernelPatcher::KextInfo IntelBTPatcher_IOUsbHostInfo {
     KernelPatcher::KextInfo::Unloaded
 };
 
-bool CIntelBTPatcher::_randomAddressInit = false;
-
 bool CIntelBTPatcher::init()
 {
     DBGLOG(DRV_NAME, "%s", __PRETTY_FUNCTION__);
     callbackIBTPatcher = this;
-    if (getKernelVersion() < KernelVersion::Monterey) {
-        lilu.onKextLoadForce(&IntelBTPatcher_IOBluetoothInfo, 1,
-        [](void *user, KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-            callbackIBTPatcher->processKext(patcher, index, address, size);
-        }, this);
-    } else {
-        lilu.onKextLoadForce(&IntelBTPatcher_IOUsbHostInfo, 1,
-        [](void *user, KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-            callbackIBTPatcher->processKext(patcher, index, address, size);
-        }, this);
-    }
+    lilu.onKextLoadForce(&IntelBTPatcher_IOUsbHostInfo, 1,
+    [](void *user, KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
+        callbackIBTPatcher->processKext(patcher, index, address, size);
+    }, this);
     return true;
 }
 
@@ -94,64 +74,23 @@ void CIntelBTPatcher::free()
 void CIntelBTPatcher::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size)
 {
     DBGLOG(DRV_NAME, "%s", __PRETTY_FUNCTION__);
-    if (getKernelVersion() < KernelVersion::Monterey) {
-        if (IntelBTPatcher_IOBluetoothInfo.loadIndex == index) {
-            DBGLOG(DRV_NAME, "%s", IntelBTPatcher_IOBluetoothInfo.id);
-            
-            KernelPatcher::RouteRequest findQueueRequestRequest {
-                "__ZN25IOBluetoothHostController17FindQueuedRequestEtP22BluetoothDeviceAddresstbPP21IOBluetoothHCIRequest",
-                newFindQueueRequest,
-                oldFindQueueRequest
-            };
-            patcher.routeMultiple(index, &findQueueRequestRequest, 1, address, size);
-            if (patcher.getError() == KernelPatcher::Error::NoError) {
-                DBGLOG(DRV_NAME, "routed %s", findQueueRequestRequest.symbol);
-            } else {
-                SYSLOG(DRV_NAME, "failed to resolve %s, error = %d", findQueueRequestRequest.symbol, patcher.getError());
-                patcher.clearError();
-            }
-            
-        }
-    } else {
-        if (IntelBTPatcher_IOUsbHostInfo.loadIndex == index) {
-            SYSLOG(DRV_NAME, "%s", IntelBTPatcher_IOUsbHostInfo.id);
-            
-            KernelPatcher::RouteRequest hostDeviceRequest {
-            "__ZN15IOUSBHostDevice13deviceRequestEP9IOServiceRN11StandardUSB13DeviceRequestEPvP18IOMemoryDescriptorRjP19IOUSBHostCompletionj",
-                newHostDeviceRequest,
-                oldHostDeviceRequest
-            };
-            patcher.routeMultiple(index, &hostDeviceRequest, 1, address, size);
-            if (patcher.getError() == KernelPatcher::Error::NoError) {
-                SYSLOG(DRV_NAME, "routed %s", hostDeviceRequest.symbol);
-            } else {
-                SYSLOG(DRV_NAME, "failed to resolve %s, error = %d", hostDeviceRequest.symbol, patcher.getError());
-                patcher.clearError();
-            }
+    if (IntelBTPatcher_IOUsbHostInfo.loadIndex == index) {
+        SYSLOG(DRV_NAME, "%s", IntelBTPatcher_IOUsbHostInfo.id);
+        
+        KernelPatcher::RouteRequest hostDeviceRequest {
+        "__ZN15IOUSBHostDevice13deviceRequestEP9IOServiceRN11StandardUSB13DeviceRequestEPvP18IOMemoryDescriptorRjP19IOUSBHostCompletionj",
+            newHostDeviceRequest,
+            oldHostDeviceRequest
+        };
+        patcher.routeMultiple(index, &hostDeviceRequest, 1, address, size);
+        if (patcher.getError() == KernelPatcher::Error::NoError) {
+            SYSLOG(DRV_NAME, "routed %s", hostDeviceRequest.symbol);
+        } else {
+            SYSLOG(DRV_NAME, "failed to resolve %s, error = %d", hostDeviceRequest.symbol, patcher.getError());
+            patcher.clearError();
         }
     }
 }
-
-#pragma mark - For Bigsur-, patch unhandled 0x2019 opcode
-
-#define HCI_OP_LE_START_ENCRYPTION 0x2019
-
-IOReturn CIntelBTPatcher::newFindQueueRequest(void *that, unsigned short arg1, void *addr, unsigned short arg2, bool arg3, void **hciRequestPtr)
-{
-    IOReturn ret = FunctionCast(newFindQueueRequest, callbackIBTPatcher->oldFindQueueRequest)(that, arg1, addr, arg2, arg3, hciRequestPtr);
-    if (ret != 0 && arg1 == HCI_OP_LE_START_ENCRYPTION) {
-        ret = FunctionCast(newFindQueueRequest, callbackIBTPatcher->oldFindQueueRequest)(that, arg1, addr, 0xffff, arg3, hciRequestPtr);
-        DBGLOG(DRV_NAME, "%s ret: %d arg1: 0x%04x arg2: 0x%04x arg3: %d ptr: %p", __FUNCTION__, ret, arg1, arg2, arg3, *hciRequestPtr);
-    }
-    return ret;
-}
-
-#pragma mark - For Monterey+ patch for intercept HCI REQ and RESP
-
-StandardUSB::DeviceRequest randomAddressRequest;
-// Hardcoded Random Address HCI Command
-const uint8_t randomAddressHci[9] = {0x05, 0x20, 0x06, 0x94, 0x50, 0x64, 0xD0, 0x78, 0x6B}; 
-IOBufferMemoryDescriptor *writeHCIDescriptor = nullptr;
 
 #define MAX_HCI_BUF_LEN             255
 #define HCI_OP_RESET                0x0c03
@@ -163,10 +102,10 @@ IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, 
     HciCommandHdr *hdr = nullptr;
     uint32_t hdrLen = 0;
     char hciBuf[MAX_HCI_BUF_LEN] = {0};
+    IOReturn ret = 0;
     
     if (data == nullptr) {
-        if (descriptor != nullptr &&
-            (getKernelVersion() < KernelVersion::Sequoia || !descriptor->prepare(kIODirectionOut))) {
+        if (descriptor != nullptr && (getKernelVersion() < KernelVersion::Sequoia || !descriptor->prepare(kIODirectionOut))) {
             if (descriptor->getLength() > 0) {
                 descriptor->readBytes(0, hciBuf, min(descriptor->getLength(), MAX_HCI_BUF_LEN));
                 hdrLen = (uint32_t)min(descriptor->getLength(), MAX_HCI_BUF_LEN);
@@ -175,37 +114,19 @@ IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, 
                 descriptor->complete(kIODirectionOut);
         }
         hdr = (HciCommandHdr *)hciBuf;
-        if (hdr->opcode == HCI_OP_LE_SET_SCAN_PARAM) {
-            if (!_randomAddressInit) {
-                randomAddressRequest.bmRequestType = makeDeviceRequestbmRequestType(kRequestDirectionOut, kRequestTypeClass, kRequestRecipientInterface);
-                randomAddressRequest.bRequest = 0xE0;
-                randomAddressRequest.wIndex = 0;
-                randomAddressRequest.wValue = 0;
-                randomAddressRequest.wLength = 9;
-                length = 9;
-                if (writeHCIDescriptor == nullptr)
-                    writeHCIDescriptor = IOBufferMemoryDescriptor::withBytes(randomAddressHci, 9, kIODirectionOut);
-                writeHCIDescriptor->prepare(kIODirectionOut);
-                IOReturn ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, randomAddressRequest, nullptr, writeHCIDescriptor, length, nullptr, timeout);
-                writeHCIDescriptor->complete();
-                const char *randAddressDump = _hexDumpHCIData((uint8_t *)randomAddressHci, 9);
-                if (randAddressDump) {
-                    SYSLOG(DRV_NAME, "[PATCH] Sending Random Address HCI %lld %s", ret, randAddressDump);
-                    IOFree((void *)randAddressDump, 9 * 3 + 1);
-                }
-                _randomAddressInit = true;
-                SYSLOG(DRV_NAME, "[PATCH] Resend LE SCAN PARAM HCI %lld", ret);
-            }
-        }
-    } else {
+    }
+    else {
         hdr = (HciCommandHdr *)data;
         hdrLen = request.wLength - 3;
     }
+    
     if (hdr) {
-        // HCI reset, we need to send Random address again
-        if (hdr->opcode == HCI_OP_RESET)
-            _randomAddressInit = false;
-#if DEBUG
+        if (hdr->opcode != HCI_OP_LE_SET_SCAN_ENABLE || hdr->data[0] != 0x00)
+            ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, data, descriptor, length, completion, timeout);
+        else
+            DBGLOG(DRV_NAME, "STOP SCAN_ENABLE SET TO 0x00!");
+        
+#if DEBUG        
         DBGLOG(DRV_NAME, "[%s] bRequest: 0x%x direction: %s type: %s recipient: %s wValue: 0x%02x wIndex: 0x%02x opcode: 0x%04x len: %d length: %d async: %d", provider->getName(), request.bRequest, requestDirectionNames[(request.bmRequestType & kDeviceRequestDirectionMask) >> kDeviceRequestDirectionPhase], requestRecipientNames[(request.bmRequestType & kDeviceRequestRecipientMask) >> kDeviceRequestRecipientPhase], requestTypeNames[(request.bmRequestType & kDeviceRequestTypeMask) >> kDeviceRequestTypePhase], request.wValue, request.wIndex, hdr->opcode, hdr->len, request.wLength, completion != nullptr);
         if (hdrLen) {
             const char *dump = _hexDumpHCIData((uint8_t *)hdr, hdrLen);
@@ -216,5 +137,7 @@ IOReturn CIntelBTPatcher::newHostDeviceRequest(void *that, IOService *provider, 
         }
 #endif
     }
-    return FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, data, descriptor, length, completion, timeout);
+    else
+        ret = FunctionCast(newHostDeviceRequest, callbackIBTPatcher->oldHostDeviceRequest)(that, provider, request, data, descriptor, length, completion, timeout);
+    return ret;
 }
